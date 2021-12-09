8a5e2d2122ff239ca3b52c2cbef24b8b56590ec2
panagiotis <pefstrat@gmail.com>
2021-12-09 09:55:18 +0100

    More details

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="AUTOMATED_ETL_POSTGRESQL",
+    name="automated-etl-3dhpe",
     version="0.0.1",
-    description="",
-    url="",
-    author="Panagiotis Ef.",
-    author_email="",
-    license="Creative Commons Zero v1.0 Universal",
+    description="""
+        An automated pipeline tool which works like that
+        --> [E]xtract images locally, [T]ranform them applying a 3D human pose estimation model
+        and [L]oad them (+ more details) into PostgreSQL database.""",
+    url="https://github.com/pan-efs/AutomatedETL_3DHPE",
+    author="Panagiotis Efstratiou",
+    author_email="pefstrat@gmail.com",
+    license="",
     packages=find_packages(),
     python_requires=">=3.8",
     include_package_data=True,
